43ed3a36e03ecdee7e1039f27a661c82fbca8708
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-10-02 16:36:37 -0400

    completed this drill

--- week02/distillery/main.rb
+++ week02/distillery/main.rb
@@ -9,34 +9,26 @@ require_relative 'person'
 
   d1 = Distillery.new("Big Brew")
   d1.brew_beverage
-
-  # Create a new person here
-  p1 = Person.new("Lucy")
-  # Take a beverage from the distillery and store it into a variable
-  bought = d1.cellar.first
-  p1.buy_beverage(bought)
-  # Give that beverage to the person
-  # have that person drink the
-  new_bev = p1.liquor_cabinet.first
-  p1.drink_beverage(new_bev)
-  # Render an image of a beer bottle to represent the beer drank
-  # Inside of this function, render an .erb template that you need to create inside of a 'views' folder
-  # Here you need to create an instance variable '@num_beverages'
-  # based on the number of beverages currently in the distillery,
-  @num_beverages = d1.beverage_count
-  # This instance variable is accessible in the erb file so that you can do the following
-  # (see the code snipped below). This will show a beer bottle for the current number of beers in the file.
-
+  d1.brew_beverage
+  d1.brew_beverage
+  d1.brew_beverage
 
 get "/brew_beverage" do
   erb :brew
 end
+
+p1 = Person.new("Lucy")
+  bought = d1.cellar.first
+  p1.buy_beverage(bought)
+  new_bev = p1.liquor_cabinet.first
+  p1.drink_beverage(new_bev)
 
 get "/drink_beverage" do
   erb :drink_it
 end
 
 get "/beverage_stock" do
+  @num_beverages = d1.beverage_count
   erb :stock
 end
 
